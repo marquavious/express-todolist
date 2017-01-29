109806e88fde534478b48d37b0aedc4ca3131f34
Marquavious Draggon <marq.draggon@gmail.com>
2017-01-29 09:55:53 -0800

    working on mongo

---
+++ models/todo.js
@@ -0,0 +1,12 @@
+var mongoose = require("mongoose")
+
+var Schema = mongoose.Schema
+
+var TodoSchema = new Schema({
+    body: String,
+    completed: Boolean,
+})
+
+var Todo = mongoose.model('Todo', TodoSchema)
+
+module.export = Todo;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express')
 var handlebars = require('express-handlebars');
 var bodyParser = require('body-parser')
 var app = express()
+var mongoose = require('mongoose')
 
 // Use `.hbs` for extensions and find partials in `views/partials`.
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
@@ -14,12 +15,6 @@ app.use(bodyParser.urlencoded({
     extended:true
 }))
 
-var todos = [
-    {body:"take out laundry",completed: false},
-    {body:"take out trash",completed: true},
-    {body:"eat",completed: false}
-
-]
 // TODOS INDEX
 app.get('/', function (req, res) {
     res.render('home', { todos: todos });
@@ -28,18 +23,30 @@ app.get('/', function (req, res) {
 // TODOS SHOW
 app.get('/todos/:id',function(req,res){
     var todo = todos[req.params.id]
-    res.render('todos-show', {todo:todo})
+    // res.render('todos-show', {todo:todo})
 })
+
+var todos = [
+    {body:"take out laundry",completed: false},
+    {body:"take out trash",completed: true},
+    {body:"eat",completed: false}
+
+]
 
 // TODOS CREATE
 app.post('/todos',function(req, res) {
     console.log(req.body);
-var todo = req.body
-todos.push(todo)
+    var todo = req.body
+Todo.create(todo, function (err,todo){
+    console.log(err);
     res.status(200).json(todo)
+    })
 })
 
 app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost/express-start');
+var Todo = require('./models/todo.js')
 
 
 app.listen(3000, function () {
