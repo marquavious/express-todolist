1da1529f228b952ed780b0955d3a3105230a0349
Marquavious Draggon <marq.draggon@gmail.com>
2017-01-29 08:09:24 -0800

    handlebars working

--- server.js
+++ server.js
@@ -5,21 +5,16 @@ var handlebars = require('express-handlebars');
 var app = express()
 
 // Use `.hbs` for extensions and find partials in `views/partials`.
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.get('/todos', function (req, res){
+app.get('/', function (req, res) {
     var todos = [
         {body:"take out laundry",completed: false},
         {body:"take out trash",completed: true},
     ]
-    res.json(todos)
-})
-
-app.get('/', function (req, res) {
-    res.render('home');
+    res.render('home', { todos: todos });
 });
-
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
