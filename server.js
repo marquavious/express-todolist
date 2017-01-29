c62ad6dc91f47b6b6a0e1aac180d8d86f9b127e8
Marquavious Draggon <marq.draggon@gmail.com>
2017-01-29 08:43:06 -0800

    adding todos to list

--- server.js
+++ server.js
@@ -2,11 +2,17 @@
 
 var express = require('express')
 var handlebars = require('express-handlebars');
+var bodyParser = require('body-parser')
 var app = express()
 
 // Use `.hbs` for extensions and find partials in `views/partials`.
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
 
 var todos = [
     {body:"take out laundry",completed: false},
@@ -14,10 +20,28 @@ var todos = [
     {body:"eat",completed: false}
 
 ]
-
+// TODOS INDEX
 app.get('/', function (req, res) {
     res.render('home', { todos: todos });
 });
+
+// TODOS SHOW
+app.get('/todos/:id',function(req,res){
+    var todo = todos[req.params.id]
+    res.render('todos-show', {todo:todo})
+})
+
+// TODOS CREATE
+app.post('/todos',function(req, res) {
+    console.log(req.body);
+var todo = req.body
+todos.push(todo)
+
+
+    res.status(200).json(todo)
+})
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
